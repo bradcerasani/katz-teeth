3fa6d915ccb9db92862c2b7d16ad470b161f1048
Brad Cerasani <bradcerasani@gmail.com>
2014-07-09 00:29:13 -0500

    branch sync

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 var myApp = angular.module('Katz',[]);
 
 myApp.service('dataService', function($http) {
-delete $http.defaults.headers.common['X-Requested-With'];
-this.getData = function(callbackFunc) {
+  delete $http.defaults.headers.common['X-Requested-With'];
+  this.getData = function(callbackFunc) {
     $http({
         method: 'GET',
         url: 'http://data.winnipeg.ca/resource/3kmj-eezt.json'
      }).success(function(data){
-        callbackFunc(data);
+        var total = 0;
+        for (var i = data.length - 1; i >= 0; i--) {
+          var entry = data[i];
+          if (entry.account === "Dental Plan") {
+            total += parseFloat(entry.amount);
+          }
+        };
+        callbackFunc(total);
     }).error(function(){
-        alert("error");
+        console.log("error");
     });
- }
+  }
 });
 
 myApp.controller('MainController', function($scope, dataService) {
-    $scope.data = null;
-    dataService.getData(function(dataResponse) {
-        $scope.data = dataResponse;
-    });
+  $scope.data = null;
+  dataService.getData(function(dataResponse) {
+    $scope.data = dataResponse;
+  });
 });
